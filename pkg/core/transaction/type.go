8bce240c5515d22835441f49acc2230ac96101d7
anthdm <anthony@academiclabs.co>
2018-03-03 10:30:21 +0100

    Refactored emit into VM package + moved tx to own package.

--- pkg/core/transaction/type.go
+++ pkg/core/transaction/type.go
@@ -6,44 +6,35 @@ type TransactionType uint8
 // All processes in NEO system are recorded in transactions.
 // There are several types of transactions.
 const (
-	MinerType      TransactionType = 0x00
-	IssueType      TransactionType = 0x01
-	ClaimType      TransactionType = 0x02
-	EnrollmentType TransactionType = 0x20
-	VotingType     TransactionType = 0x24
-	RegisterType   TransactionType = 0x40
-	ContractType   TransactionType = 0x80
-	StateType      TransactionType = 0x90
-	AgencyType     TransactionType = 0xb0
-	PublishType    TransactionType = 0xd0
-	InvocationType TransactionType = 0xd1
+	MinerTX      TransactionType = 0x00
+	IssueTX      TransactionType = 0x01
+	ClaimTX      TransactionType = 0x02
+	EnrollmentTX TransactionType = 0x20
+	VotingTX     TransactionType = 0x24
+	RegisterTX   TransactionType = 0x40
+	ContractTX   TransactionType = 0x80
+	AgencyTX     TransactionType = 0xb0
 )
 
 // String implements the stringer interface.
 func (t TransactionType) String() string {
 	switch t {
-	case MinerType:
+	case MinerTX:
 		return "miner transaction"
-	case IssueType:
+	case IssueTX:
 		return "issue transaction"
-	case ClaimType:
+	case ClaimTX:
 		return "claim transaction"
-	case EnrollmentType:
+	case EnrollmentTX:
 		return "enrollment transaction"
-	case VotingType:
+	case VotingTX:
 		return "voting transaction"
-	case RegisterType:
+	case RegisterTX:
 		return "register transaction"
-	case ContractType:
+	case ContractTX:
 		return "contract transaction"
-	case StateType:
-		return "state transaction"
-	case AgencyType:
+	case AgencyTX:
 		return "agency transaction"
-	case PublishType:
-		return "publish transaction"
-	case InvocationType:
-		return "invocation transaction"
 	default:
 		return ""
 	}
